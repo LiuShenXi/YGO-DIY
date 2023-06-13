be84d2d84c510b4fcab20de23287b6a3ee1f8cdd
shenxi <lfxjohnliu@163.com>
2023-06-13 12:43:48 +0800

    feat: 搭建基础路由

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,14 @@
-export default function Home() {
-  return (
-    <div>
-      <h1>Todos</h1>
-    </div>
-  );
+import { useRouter } from 'next/router'
+import { useEffect } from 'react'
+
+const HomePage: React.FC = () => {
+  const router = useRouter()
+
+  useEffect(() => {
+    router.replace('/my-cards')
+  }, [])
+
+  return null
 }
+
+export default HomePage
---
+++ src/pages/my-cards/index.tsx
@@ -0,0 +1,7 @@
+export default function Home() {
+  return (
+    <div>
+      <h1>My cards</h1>
+    </div>
+  );
+}
