d6f8f612aef0708bfd9838d5607838d6c217c6b3
shenxi <lfxjohnliu@163.com>
2023-06-16 17:30:31 +0800

    feat: 删除 reset

--- src/components/Menu/index.tsx
+++ src/components/Menu/index.tsx
@@ -1,7 +1,7 @@
 const Menu = () => {
   return (<div>
-    <ul className="menu w-56 rounded-box bg-neutral text-neutral-content">
-      <li className="menu-title">Title</li>
+    <ul className="menu w-56 bg-neutral text-neutral-content">
+      <li className="menu-title">套牌列表</li>
       <li><a>Item 1</a></li>
       <li><a>Item 2</a></li>
       <li><a>Item 3</a></li>
