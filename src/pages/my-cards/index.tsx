54bf231a1aafbc80b485c2dfe58b1014f291141e
shenxi <lfxjohnliu@163.com>
2023-06-20 17:32:48 +0800

    feat: some leyout coding

--- src/pages/my-cards/index.tsx
+++ src/pages/my-cards/index.tsx
@@ -2,14 +2,17 @@ import Menu from '@/components/Menu'
 import Card from '@/components/Card'
 import styles from './index.module.css'
 import classnames from 'classnames'
+import { useState } from 'react'
 
 export default function MyCards() {
+  const [duckName, useDeckName] = useState('E-Hero')
   return (
     <div className={classnames('root-content',styles.cardWp)}>
       <div>
         <Menu></Menu>
       </div>
-      <div className={classnames(styles.Waterfall, 'container', 'mx-auto', 'px-4')}>
+      <div className={classnames(styles.Waterfall, 'container', 'mx-auto')}>
+        <p className={styles.deckName}>{duckName}</p>
         <Card />
       </div>
     </div>
